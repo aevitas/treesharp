6fda960d79fc95cd5300e07b2d6751bbbe55e2f3
apocdev <apocdev@94b08b57-0ae1-3da8-8c2e-58ed663b5133>
2010-03-23 21:54:29 +0000

    Mostly finished implementation. Now includes the ability to use delegates for decorators, and actions. Parallel implementation needs more thought before I actually implement it.

--- TreeSharp/Parallel.cs
+++ TreeSharp/Parallel.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#region License
+
+//     A simplistic Behavior Tree implementation in C#
+//     Copyright (C) 2010  ApocDev apocdev@gmail.com
+// 
+//     This file is part of TreeSharp.
+// 
+//     TreeSharp is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     TreeSharp is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
 
 namespace TreeSharp
 {
-    class Parallel : Composite
+    internal class Parallel : Composite
     {
     }
 }
--- TreeSharp/Sequence.cs
+++ TreeSharp/Sequence.cs
@@ -1,16 +1,62 @@
-﻿using System;
+﻿#region License
+
+//     A simplistic Behavior Tree implementation in C#
+//     Copyright (C) 2010  ApocDev apocdev@gmail.com
+// 
+//     This file is part of TreeSharp.
+// 
+//     TreeSharp is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     TreeSharp is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace TreeSharp
 {
     /// <summary>
-    /// The base sequence class. This will execute each branch of logic, in order.
-    /// If all branches succeed, this composite will return a successful run status.
-    /// If any branch fails, this composite will return a failed run status.
+    ///   The base sequence class. This will execute each branch of logic, in order.
+    ///   If all branches succeed, this composite will return a successful run status.
+    ///   If any branch fails, this composite will return a failed run status.
     /// </summary>
-    public class Sequence : Composite
+    public class Sequence : GroupComposite
     {
+        public Sequence(params Composite[] children) : base(children)
+        {
+        }
+
+        public override IEnumerable<RunStatus> Execute(object context)
+        {
+            foreach (Composite node in Children)
+            {
+                node.Start(context);
+                while (node.Tick(context) == RunStatus.Running)
+                {
+                    Selection = node;
+                    yield return RunStatus.Running;
+                }
+
+                Selection = null;
+                node.Stop(context);
+
+                if (node.LastStatus == RunStatus.Failure)
+                {
+                    yield return RunStatus.Failure;
+                    yield break;
+                }
+                yield return RunStatus.Running;
+            }
+            yield return RunStatus.Success;
+        }
     }
 }
