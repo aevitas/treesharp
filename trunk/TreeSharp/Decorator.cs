66c86be2d39ffdffaedc2a0772af8d708bf08a75
apocdev <apocdev@94b08b57-0ae1-3da8-8c2e-58ed663b5133>
2010-03-23 20:43:06 +0000

    Initial commit. Basic structure laid out.

---
+++ trunk/TreeSharp/Decorator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TreeSharp
+{
+    public abstract class Decorator : Composite
+    {
+
+    }
+
+    public abstract class InverseDecorator : Decorator
+    {
+        
+    }
+}
